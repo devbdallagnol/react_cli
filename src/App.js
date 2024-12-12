66ffcf9aa7db82cfc7bb71c9e3b3bc62efcaa8a0
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-12 16:01:47 -0300

    added - import BrowserRouter, Routes, Route - added login, home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+
 function App() {
   return (
-    <div className="App">
-      React CLI
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+      <>
+        <h1>Login</h1>
+        <button>Voltar para home</button>
+      </>);
+  };
+  
+  export { Login };
