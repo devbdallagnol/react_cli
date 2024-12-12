3a690a495ebb74d0df75caf701cf6558e72b7a3f
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-12 16:57:14 -0300

    added - import Button and test button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 
+import { Button } from '../../components/Button';
+
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Button title="teste 01">Teste 1</Button>
+      <Button title="teste 02" variant='secondary'>Teste 2</Button>
       <Link to="/login">Fazer login</Link>
     </>);
 };
