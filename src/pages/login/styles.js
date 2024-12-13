20df0eb72f69e62de02afa2d5ac560f0f57e6841
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-13 10:40:46 -0300

    updated - adjust login page

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.main`
     width: 100%;
@@ -12,30 +12,71 @@ export const Container = styled.main`
     align-items: center;
 `
 
+export const Wrapper = styled.div`
+    max-width: 300px;
+`
+
+export const Column = styled.div`
+    flex: 1;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+`
+
+
+
 export const Title = styled.h2`
-    font-family: 'Opens-Sans';
+    font-family: 'Open Sans';
     font-style: normal;
     font-weight: 700;
     font-size: 32px;
-    width: 320px;
+    max-width: 90%;
     margin-bottom: 20px;
     line-height: 44px;
 
-    color: #fff;
+    color: #FFFFFF;
 `
 
-export const TitleHightLight = styled.span`
-    color: #e41050;
+export const TitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 44px;
+    margin-bottom: 8px;
 `
 
-export const TextContent = styled.p`
-    font-family: 'Opens-Sans';
+export const SubtitleLogin = styled.p`
+    font-family: 'Open Sans';
     font-style: normal;
     font-weight: 400;
-    font-size: 16px;
-    width: 420px;
-    margin-bottom: 20px;
-    line-height: 22px;
+    font-size: 18px;
+    line-height: 25px;
+    margin-bottom: 35px;
+`
 
-    color: #fff;
+
+export const EsqueciText  = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+
+    color: #E5E044;
+` 
+
+export const CriarText =  styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+
+    color: #23DD7A;
 `
