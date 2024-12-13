d3388583a7748fb646fb2c0caa26b6666a3710bb
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-13 11:30:30 -0300

    useEffect - quando algo sofre alteracao, muda o estado do componente

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Teste = () => {
+  // useState  
   const [name, setName] = useState("Bruno");
 
   const handleChangeName = () => {
     setName(prev => prev === "José" ? 'Augusto' : 'Pablo');
   };
 
+  //useEffect
+//   useEffect(() => {
+//     alert("Renderizei") // renderiza 1x ou se name sofrer alteracao
+//   }, [name]);
+
+  const changeUser = () => {
+    handleChangeName();
+  }
+  useEffect(() => {
+    handleChangeName();
+  }, [])
+
+
   return (<>
+
   <div>
+    <h3>useState</h3>
     <p>{name}</p>
   </div>
   <button onClick={handleChangeName}>Alterar</button>
+  <hr />
   </>)};
 
 export { Teste };
