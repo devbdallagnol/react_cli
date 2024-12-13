0a357df917c27b479f0e5ca51c3a042033e0e2b8
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-13 10:19:57 -0300

    added - import Button and Input

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import { Container, TextContent, Title, TitleHightLight } from "./styles";
 
 const Login = () => {
-    return (
-      <>
-        <h1>Login</h1>
-        <Link to="/">Voltar para home</Link>
-      </>);
-  };
-  
-  export { Login };
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHightLight>
+              Implemente <br />
+            </TitleHightLight>
+            o seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadoras do
+            mundo e encare seu novo desafio profissional, evoluindo em
+            comunidade com os melhores experts.
+          </TextContent>
+          <Button
+            title="Começar agora"
+            variant="secondary"
+            
+          />
+        </div>
+        <div>
+          <Input placeholder="email"/>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export { Login };
