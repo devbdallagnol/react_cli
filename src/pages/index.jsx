6a7ad529fdb6240245cf4da50f1b5b77fbf9fe91
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-18 14:13:44 -0300

    useCallBack - memoriza valor na funcao para nao ficar re-renderizando

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,36 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 const Teste = () => {
   // useState
   const [age, setAge] = useState(36);
+  const [name, setName] = useState("Bruno");
 
-  const handleChangeName = () => {
+  const handleChangeName = useCallback(() => {
+    setName((prev) => (prev === "Bruno" ? "José" : "Bruno"));
+  }, []);
+
+  const handleChangeAge = useCallback(() => {
+    const newAge = 10 * age;
+    console.log("AgeAtual: ", age, " | NewAge: ", newAge)
     setAge((prev) => (prev === 36 ? 32 : 36));
-  };
+  }, []);
 
   const calculo = useMemo(() => {
     console.log("calculou!", age);
     return 10 * age;
   }, [age]);
 
-  console.log("renderizou", calculo);
-
   return (
     <div>
       <h3>useState</h3>
-      <p>{age}</p>
-      <button onClick={handleChangeName}>Alterar</button>
+      <div>
+        <p>Idade: {age}</p>
+      </div>
+      <div>
+        <p>Nome: {name}</p>
+      </div>
+      <button onClick={handleChangeAge}>Alterar Idade</button>
+      <button onClick={handleChangeName}>Alterar Nome</button>
     </div>
   );
 };
